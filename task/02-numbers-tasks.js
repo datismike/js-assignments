f9e04e695ed6e3a2f2a126c99edda5765820b7f7
datismike <47022825+datismike@users.noreply.github.com>
2019-04-21 17:00:43 +0300

    Update 02-numbers-tasks.js

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -161,7 +161,7 @@ function parseNumberFromString(value) {
  *   1,2,3   => 3.741657386773941
  */
 function getParallelipidedDiagonal(a,b,c) {
-    return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+    return Math.sqrt(a * a + b * b + c * c);
 }
 
 /**

97c34a1504859cf022d55a01e72219ea1c3caffa
datismike <47022825+datismike@users.noreply.github.com>
2019-04-21 18:01:25 +0300

    Update 03-date-tasks.js

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,10 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+    let year = date.getFullYear();
+    if (year % 4 !== 0) return false;
+    else if (year % 100 !== 0) return true;
+    else return year % 400 === 0;
 }
 
 
@@ -94,7 +97,20 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    let hours = date.getUTCHours();
+    if (hours > 12) {
+        hours -= 12;
+    }
+    let hourDegree = (hours + (date.getUTCMinutes() / 60)) * 30;
+    if (hourDegree > 180) {
+        hourDegree -= 180;
+    }
+
+    let minDegree = date.getUTCMinutes() * 6;
+    if (minDegree > 180 ) {
+        minDegree -= 180;
+    }
+    return (Math.abs((hourDegree - minDegree )) * Math.PI) / 180;
 }
 
 
